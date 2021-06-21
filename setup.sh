9929cd552facd1225d025e46736dd31634073022
Jaime Martinez <panchitoboy@gmail.com>
2021-06-21 09:55:21 +0200

    Add node

--- setup.sh
+++ setup.sh
@@ -1,5 +1,45 @@
 ## Node.JS LTS
 
-echo "Installing Node 14 JS LTS"
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt-get install -y nodejs 
+
+install_node() {
+    echo "Installing Node 14 JS LTS"
+    
+    curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+    sudo apt-get install -y nodejs
+    mkdir ~/.npm-global
+    npm config set prefix '~/.npm-global'
+    echo '' >> ~/.bashrc
+    echo 'PATH=~/.npm-global/bin:$PATH' | tee -a ~/.bashrc
+    echo '' >> ~/.bashrc
+}
+
+install_git() {
+    echo "Installing GIT"
+
+    sudo apt-get install -y git gitk
+    
+    echo "Please write git name:"
+    read GIT_NAME
+
+    echo "Please write git email:"
+    read GIT_EMAIL
+
+    git config --global user.name $GIT_NAME
+    git config --global user.email $GIT_EMAIL
+    git config --global core.editor "code --wait"
+    git config --global merge.ff only
+
+    echo '' >> ~/.bashrc
+    echo 'force_color_prompt=yes' >> ~/.bashrc
+    echo 'color_prompt=yes' >> ~/.bashrc
+    echo 'parse_git_branch() {' >> ~/.bashrc
+    echo ' git branch 2> /dev/null | sed -e '\''/^[^*]/d'\'' -e '\''s/* \(.*\)/(\\1)/'\'' ' >> ~/.bashrc
+    echo '}' >> ~/.bashrc
+    echo 'if [ "$color_prompt" = yes ]; then' >> ~/.bashrc
+    echo ' PS1='\''${debian_chroot:+($debian_chroot)}\[\\033[01;32m\]\u@\h\[\\033[00m\]:\[\\033[01;34m\]\w\[\\033[01;31m\]$(parse_git_branch)\[\\033[00m\]\$ '\'' ' >> ~/.bashrc
+    echo 'else' >> ~/.bashrc
+    echo ' PS1='\''${debian_chroot:+($debian_chroot)}\u@\h:\w$(parse_git_branch)\$ '\'' ' >> ~/.bashrc
+    echo 'fi' >> ~/.bashrc
+    echo 'unset color_prompt force_color_prompt' >> ~/.bashrc
+    echo '' >> ~/.bashrc
+}
