dc591cf303523cb8cd4e42b704befad2a224f044
Jaime Martinez <panchitoboy@gmail.com>
2021-06-21 10:36:54 +0200

    setup-wsl.sh

---
+++ setup-wsl.sh
@@ -0,0 +1,14 @@
+
+install_intellij() {
+    sudo rm -rf /opt/intellij
+    sudo mkdir /opt/intellij
+    wget https://download.jetbrains.com/idea/ideaIU-2021.1.2.tar.gz
+    tar -xvzf ideaIU-2021.1.2.tar.gz
+    rm ideaIU-2021.1.2.tar.gz
+    sudo mv idea-I**/* /opt/intellij
+    rm -rf idea-I*
+    echo "alias intellij='nohup sh /opt/intellij/bin/idea.sh > /dev/null 2>&1&'" >> ~/.bashrc
+    # lombook plugin 
+    # eclipse formatter 
+    # maven settings
+}
